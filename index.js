fb166db8392db368cd8d44e6ed95ef6e2ed70afc
Hoon Lee <yl.hoony@gmail.com>
2017-10-07 02:55:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+ function justInvoke(fn) {
+   return fn();
+ }
+
+ function setThisWithCall(fn, thisValue) {
+   return fn.call(thisValue);
+ }
+
+ function setThisWithApply(fn, thisValue, args) {
+   return args ?  fn.apply(thisValue, [...args]) : fn.apply(thisValue);
+ }
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  let newFunction = functionToBeCopied.bind(thisValue);
+  return newFunction;
+}
